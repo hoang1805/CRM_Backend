242e865d0903a3e19b3b8ad8d58e58830e078983
CDQuan88 <duyquan7b@gmail.com>
2025-02-17 20:43:25 +0700

    Add table Task and Feedback

--- crm.sql
+++ crm.sql
@@ -1,137 +1,234 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+CREATE DATABASE  IF NOT EXISTS `crm` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `crm`;
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 14, 2025 at 06:13 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: crm
+-- ------------------------------------------------------
+-- Server version	9.2.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `crm`
---
-CREATE DATABASE IF NOT EXISTS `crm` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `crm`;
-
--- --------------------------------------------------------
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `accounts`
 --
 
-CREATE TABLE IF NOT EXISTS `accounts` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `assigned_user_id` bigint(20) DEFAULT NULL,
-  `code` varchar(255) DEFAULT NULL,
-  `created_at` bigint(20) DEFAULT NULL,
-  `creator_id` bigint(20) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `birthday` bigint(20) NOT NULL,
-  `gender` varchar(255) DEFAULT NULL,
-  `job` varchar(255) DEFAULT NULL,
-  `last_update` bigint(20) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT NULL,
-  `relationship_id` bigint(20) DEFAULT NULL,
-  `source_id` bigint(20) DEFAULT NULL,
-  `referrer_id` bigint(20) DEFAULT NULL,
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `accounts` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `assigned_user_id` bigint DEFAULT NULL,
+  `code` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `created_at` bigint DEFAULT NULL,
+  `creator_id` bigint DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `birthday` bigint NOT NULL,
+  `gender` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `job` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `last_update` bigint DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `relationship_id` bigint DEFAULT NULL,
+  `source_id` bigint DEFAULT NULL,
+  `referrer_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `relationship_id` (`relationship_id`),
-  KEY `code_2` (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `code_2` (`code`),
+  FULLTEXT KEY `code` (`code`,`name`,`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (1,1,'12313',1739602037795,1,'quancdhe176693@fpt.edu.vn',1739601958164,'MALE','',1739602037795,'Cao Duy Quân','0355771203',1,2,1);
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `feedbacks`
+--
+
+DROP TABLE IF EXISTS `feedbacks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `feedbacks` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `content` text COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` bigint DEFAULT NULL,
+  `account_id` bigint DEFAULT NULL,
+  `creator_id` bigint DEFAULT NULL,
+  `rating` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feedbacks`
+--
+
+LOCK TABLES `feedbacks` WRITE;
+/*!40000 ALTER TABLE `feedbacks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `feedbacks` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `relationships`
 --
 
-CREATE TABLE IF NOT EXISTS `relationships` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `color` varchar(255) DEFAULT NULL,
-  `created_at` bigint(20) DEFAULT NULL,
-  `creator_id` bigint(20) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `last_update` bigint(20) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
+DROP TABLE IF EXISTS `relationships`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `relationships` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `color` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `created_at` bigint DEFAULT NULL,
+  `creator_id` bigint DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `last_update` bigint DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `relationships`
+--
+
+LOCK TABLES `relationships` WRITE;
+/*!40000 ALTER TABLE `relationships` DISABLE KEYS */;
+INSERT INTO `relationships` VALUES (1,'#1677ff',1739601722513,1,'No',1739601722513,'Cấp trên');
+/*!40000 ALTER TABLE `relationships` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `sources`
 --
 
-CREATE TABLE IF NOT EXISTS `sources` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `code` varchar(255) DEFAULT NULL,
-  `created_at` bigint(20) DEFAULT NULL,
-  `creator_id` bigint(20) DEFAULT NULL,
-  `last_update` bigint(20) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `parent_id` bigint(20) DEFAULT NULL,
+DROP TABLE IF EXISTS `sources`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sources` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `created_at` bigint DEFAULT NULL,
+  `creator_id` bigint DEFAULT NULL,
+  `last_update` bigint DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `parent_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `code_2` (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `code_2` (`code`),
+  FULLTEXT KEY `code` (`name`,`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `sources`
+--
+
+LOCK TABLES `sources` WRITE;
+/*!40000 ALTER TABLE `sources` DISABLE KEYS */;
+INSERT INTO `sources` VALUES (1,'1111',1739601558776,1,1739601558776,'CDQuan88',NULL),(2,'1211',1739601952729,1,1739601952729,'Vanh',1);
+/*!40000 ALTER TABLE `sources` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tasks` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` text COLLATE utf8mb4_general_ci,
+  `description` text COLLATE utf8mb4_general_ci,
+  `project` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `material` text COLLATE utf8mb4_general_ci,
+  `created_at` bigint DEFAULT NULL,
+  `finished_at` bigint DEFAULT NULL,
+  `last_update` bigint DEFAULT NULL,
+  `status` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `note` text COLLATE utf8mb4_general_ci,
+  `account_id` bigint DEFAULT NULL,
+  `participant_id` bigint DEFAULT NULL,
+  `manager_id` bigint DEFAULT NULL,
+  `creator_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tasks_participant` (`participant_id`) /*!80000 INVISIBLE */,
+  FULLTEXT KEY `tasks_name` (`name`) /*!80000 INVISIBLE */,
+  FULLTEXT KEY `tasks_des` (`description`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT NULL,
-  `email` varchar(255) NOT NULL,
-  `birthday` bigint(20) NOT NULL,
-  `gender` varchar(255) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `role` varchar(255) NOT NULL,
-  `sign` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `creator_id` bigint(20) DEFAULT NULL,
-  `created_at` bigint(20) DEFAULT NULL,
-  `last_update` bigint(20) DEFAULT NULL,
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `birthday` bigint NOT NULL,
+  `gender` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `role` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `sign` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `creator_id` bigint DEFAULT NULL,
+  `created_at` bigint DEFAULT NULL,
+  `last_update` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `users_email` (`email`),
   KEY `users_username` (`username`) USING BTREE,
-  KEY `users_username_email` (`username`,`email`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `users_username_email` (`username`,`email`) USING BTREE,
+  FULLTEXT KEY `search_username_name_fts` (`username`,`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `users`
 --
 
---
--- Indexes for table `accounts`
---
-ALTER TABLE `accounts` ADD FULLTEXT KEY `code` (`code`,`name`,`phone`);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','Admin','987654654','admin',0,'OTHER','Admin','ADMIN','','$2a$12$T787.VCC4eVH6Rz2hyV6KedZr9zII.aXfGIHPQ.72m0jpJmlAnx3W',0,1739600934630,1739600934630);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for table `sources`
---
-ALTER TABLE `sources` ADD FULLTEXT KEY `code` (`name`,`code`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users` ADD FULLTEXT KEY `search_username_name_fts` (`username`,`name`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-17 20:27:06
