ba0d93ed02521996dea4d8bc1a7d3e01c6d5ea0d
hoang1805 <dhhoang203@gmail.com>
2025-02-24 18:02:48 +0700

    send mail

--- src/main/java/com/example/crm_backend/services/EmailService.java
+++ src/main/java/com/example/crm_backend/services/EmailService.java
@@ -9,16 +9,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+    private final JavaMailSender mail_sender;
+
     @Autowired
-    private JavaMailSender mail_sender;
+    public EmailService(JavaMailSender mailSender) {
+        mail_sender = mailSender;
+    }
 
     public void sendEmail(String to, String subject, String content) throws MessagingException {
         MimeMessage message = mail_sender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, "UTF-8");
 
+        helper.setFrom("your-email@gmail.com");
         helper.setTo(to);
         helper.setSubject(subject);
-        helper.setText(content, true);
+        helper.setText(content, true); // Nội dung hỗ trợ HTML
 
         mail_sender.send(message);
     }
